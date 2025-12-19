610b04b9995875576038b4c7c5678f2a22f7a546
Antoine Augsburger <aa.augsburger@gmail.com>
2025-12-19 19:06:35 +0100

    ajout fichier SnakeKeyboard

--- src/Classes/SnakeKeyboard.scala
+++ src/Classes/SnakeKeyboard.scala
@@ -1,5 +1,23 @@
-package Classes
+import hevs.graphics.FunGraphics
 
-class SnakeKeyboard {
+import java.awt.event.{KeyAdapter, KeyEvent}
 
+class SnakeKeyboard(val fg: FunGraphics) {
+
+  // Gestionnaire de clavier
+  def getKeyPressed(): Char = {
+    var key: Char = ' '
+    fg.setKeyManager(new KeyAdapter() {
+      override def keyPressed(e: KeyEvent): Unit = {
+        e.getKeyCode match {
+          case KeyEvent.VK_A | KeyEvent.VK_LEFT => key = 'a'
+          case KeyEvent.VK_S | KeyEvent.VK_DOWN => key = 's'
+          case KeyEvent.VK_W | KeyEvent.VK_UP => key = 'w'
+          case KeyEvent.VK_D | KeyEvent.VK_RIGHT=> key = 'd'
+        }
+      }
+    })
+    key
+  }
 }
+
