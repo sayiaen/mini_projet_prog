3f78b6ca240620ac7263b83788453e1a0c8a8647
tonio <aa.augsburger@gmail.com>
2025-12-22 18:05:48 +0100

    mise dans classe game gestion des collision et amélioration classe snake

--- src/Classes/Game.scala
+++ src/Classes/Game.scala
@@ -1,106 +1,67 @@
-import Classes.Grid
+import Classes.{Grid, Snake}
 
 class Game {
 
   val grid = new Grid()
   val disp = new Display(grid)
   val spd = new GameSpeed(150)
+  val snake = new Snake(grid)
 
-  val LOGIC_SPEED: Int = 200 // temps du jeu en milliseconde
 
   // Variables d'état pour les directions
-  var isAlive: Boolean = true
-  var direction: Int = 1
-  var speed: Int = 1
-  var length = 3
   var command: Int = 0
-  var posX = 20
-  var posY = 20
-
 
 
   grid.printGrid
-    while(true) {
-
-      if(spd.checkTick()) {
-        grid.updateGrid()
-        if(disp.keyInput.isSpacePressed) length += 1
-        if(isAlive) moveSnake() else   grid.end()
-
-      }
-      disp.refresh
+  while (true) {
+    if (spd.checkTick()) {
+      grid.updateGrid()
+      if (disp.keyInput.isSpacePressed) snake.length += 1
+      if (snake.isAlive) checkCollision(snake.move(inputDirection())) else grid.end()
+    }
+    disp.refresh
 
 
   }
 
   //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    def commandSnake(): Int = {
-      var outCommand: Int = direction
-      var directionChanged: Boolean = false
-      if (disp.keyInput.isUpPressed) outCommand = 1
-      if (disp.keyInput.isDownPressed) outCommand = 3
-      if (disp.keyInput.isLeftPressed) outCommand = 4
-      if (disp.keyInput.isRightPressed) outCommand = 2
-      if(outCommand != direction) directionChanged = true
-      if (directionChanged) outCommand
-      else direction
-    }
-
-    def nextDirection(): Int = {
-      var nextDirection = commandSnake()
-      if(Math.abs(nextDirection - direction) == 2) nextDirection = direction
-      nextDirection
-    }
-
-    def moveSnake(): Unit = {
-      grid.setCell(posX, posY, 'O', length)
-        nextDirection() match {
-        case 1 => posX = posX - speed
-        case 2 => posY = posY + speed
-        case 3 => posX = posX + speed
-        case 4 => posY = posY - speed
-        case _ => None
-      }
-      if(posX < 0) posX = grid.SIZE-1
-      if(posX >= grid.SIZE) posX = 0
-      if (posY < 0) posY = grid.SIZE-1
-      if (posY >= grid.SIZE) posY = 0
-
-      grid.getCell(posX, posY).cellType match {
-        case 'O' => isAlive = false; println("Tu as touché ton corps")
-        case 'F' => length += 1; println("Tu as mangé une pomme")
-        case  _ => None
-      }
-      if(isAlive) {
-        grid.setCell(posX, posY, 'T')
-        direction = nextDirection()
-      }
-      else {
-          speed = 0
-          println("Tu es mort")
-      }
+  def inputDirection(): Int = {
+    var outCommand = snake.direction
+    if (disp.keyInput.isUpPressed) outCommand = 1
+    if (disp.keyInput.isDownPressed) outCommand = 3
+    if (disp.keyInput.isLeftPressed) outCommand = 4
+    if (disp.keyInput.isRightPressed) outCommand = 2
+    outCommand
   }
 
-}
-
-
-
-class GameSpeed(val LOGIC_SPEED: Int) {
-
-  var lastTime = System.currentTimeMillis()
-
-  def checkTick(): Boolean = {
-    if(System.currentTimeMillis() - lastTime > LOGIC_SPEED) {
-      lastTime = System.currentTimeMillis()
-      true
+  def checkCollision(pos: (Int, Int, Int)): Unit = {
+    grid.getCell(pos._1, pos._2).cellType match {
+      case 'O' => snake.isAlive = false; println("Tu as touché ton corps")
+      case 'F' => snake.length += 1; println("Tu as mangé une pomme")
+      case _ => None
     }
-    else false
+    if (snake.isAlive) {
+      grid.setCell(pos._1, pos._2, 'T')
+      snake.direction = snake.nextDirection(pos._3)
+    }
+    else {
+      snake.speed = 0
+      println("Tu es mort")
+    }
+  }
+
+
+  class GameSpeed(val LOGIC_SPEED: Int = 200) {
+
+    var lastTime = System.currentTimeMillis()
+
+    def checkTick(): Boolean = {
+      if (System.currentTimeMillis() - lastTime > LOGIC_SPEED) {
+        lastTime = System.currentTimeMillis()
+        true
+      }
+      else false
+    }
   }
 }
-
-
-//        if(disp.keyInput.isUpPressed) posY -= 1
-//        if(disp.keyInput.isDownPressed) posY += 1
-//        if(disp.keyInput.isLeftPressed) posX -= 1
-//        if(disp.keyInput.isRightPressed) posX += 1
--- src/Classes/Snake.scala
+++ src/Classes/Snake.scala
@@ -1,11 +1,49 @@
 package Classes
 
-class Snake() {
+class Snake(val grid: Grid, var posX: Int = 20, var posY: Int = 20) {
   var isAlive: Boolean = true
-  var length: Int = 10
-  var speedX: Int = 1
-  var speedY: Int = 1
-  var posX: Int = 40
-  var posY: Int = 40
+  var direction: Int = 1
+  var speed: Int = 1
+  var length = 3
 
-}
+  def commandSnake(inputNextDirection: Int): Int = {
+    var directionChanged: Boolean = false
+    if (inputNextDirection != direction) directionChanged = true
+    if (directionChanged) inputNextDirection
+    else direction
+  }
+
+  def nextDirection(inputNextDirection: Int): Int = {
+    var nextDirection = commandSnake(inputNextDirection)
+    if (Math.abs(nextDirection - direction) == 2) nextDirection = direction
+    nextDirection
+  }
+
+  def move(inputNextDirection: Int = direction): (Int, Int, Int) = {
+    grid.setCell(posX, posY, 'O', length)
+    nextDirection(inputNextDirection) match {
+      case 1 => posX = posX - speed
+      case 2 => posY = posY + speed
+      case 3 => posX = posX + speed
+      case 4 => posY = posY - speed
+      case _ => None
+    }
+    if (posX < 0) posX = grid.SIZE - 1
+    if (posX >= grid.SIZE) posX = 0
+    if (posY < 0) posY = grid.SIZE - 1
+    if (posY >= grid.SIZE) posY = 0
+
+    (posX, posY, inputNextDirection)
+    }
+
+  def grow(qty: Int): Unit = {
+    length += qty
+  }
+
+  def die(): Unit =
+    {
+      isAlive = false
+    }
+
+  }
+
